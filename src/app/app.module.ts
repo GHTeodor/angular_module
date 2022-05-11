387894dbb4cb432b56121d2d977faa8595f356ab
GHTeodor <Theodor.Lviv@gmail.com>
2022-05-11 17:11:33 +0300

    Components, services, models

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,45 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http';
+import {RouterModule, Routes} from '@angular/router';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {
+  UsersComponent,
+  UserComponent,
+  PostsComponent,
+  PostComponent,
+  CommentsComponent,
+  CommentComponent
+} from './components';
+
+const routes: Routes = [
+  {path: '', redirectTo: 'users', pathMatch: 'full'},
+  {path: 'users', component: UsersComponent},
+  {path: 'users/:id', component: UserComponent},
+  {path: 'posts', component: PostsComponent},
+  {path: 'posts/:id', component: PostComponent},
+  {path: 'comments', component: CommentsComponent},
+  {path: 'comments/:id', component: CommentComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent,
+    UserComponent,
+    PostsComponent,
+    PostComponent,
+    CommentsComponent,
+    CommentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
